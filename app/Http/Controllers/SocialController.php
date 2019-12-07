fb6f411fdc08bde81dd4a8d0054cb49d60d0d823
Mirix <mirix@list.ru>
2019-12-07 22:35:13 +0300

    fix

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\UsersProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\UsersProfile;
 
 class SocialController extends Controller
 {
     public function show()
     {
-        $userProfile = UsersProfile::where('user_id', 2)->get();
+        $userProfile = UsersProfile::where('user_id', 1)->value('user_id');
 
-        dd($userProfile);
-        exit();
-
-        foreach ($userProfile as $userData) {
-//            return view('/social')->with([
-//                'name' => $userData->name,
-//                'surname' => $userData->surname,
-//                'patronymic' => $userData->patronymic,
-//                'birthday' => $userData->birthday,
-//                'status' => $userData->status,
-//                'img_path' => $userData->avatar_img,
-//            ]);
+        if (!empty($userProfile)) {
+            foreach (UsersProfile::where('user_id', 1)->get() as $userData) {
+                return view('/social')->with([
+                    'name' => $userData->name,
+                    'surname' => $userData->surname,
+                    'patronymic' => $userData->patronymic,
+                    'birthday' => $userData->birthday,
+                    'status' => $userData->status,
+                    'img_path' => $userData->avatar_img,
+                ]);
+            }
+        } else {
+            return view('/social');
         }
     }
 }
